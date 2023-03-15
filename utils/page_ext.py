44ac7034eca4599a78b98c661d9453e92cd53573
顾剑波 <gujianbobo@gmail.com>
2023-03-15 10:53:25 +0800

    ext

--- utils/page_ext.py
+++ utils/page_ext.py
@@ -1,4 +1,5 @@
 import requests
+from lxml import etree
 
 
 class PageExtraction:
@@ -8,3 +9,10 @@ class PageExtraction:
     def get_html_text(self, url):
         html = requests.get(url, headers=self.head)
         return html.text
+
+    def get_info(self, url):
+        html_text = self.get_html_text(url)
+        selector = etree.HTML(html_text)
+        title = selector.xpath("//h1[@class='citation__title']/text()")
+        print(title)
+
