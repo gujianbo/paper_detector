1329e4543390bbda4fcbb3050424e014815c8df7
顾剑波 <gujianbobo@gmail.com>
2023-03-15 11:02:34 +0800

    ext

--- utils/page_ext.py
+++ utils/page_ext.py
@@ -14,5 +14,23 @@ class PageExtraction:
         html_text = self.get_html_text(url)
         selector = etree.HTML(html_text)
         title = selector.xpath("//h1[@class='citation__title']/text()")
-        print(title)
+        author = selector.xpath("//span[@class='loa__author-name']/span/text()")
+        conference = selector.xpath("//span[@class='epub-section__title']/text()")
+        date = selector.xpath("//span[@class='epub-section__date']/text()")
+        page = selector.xpath("//span[@class='epub-section__pagerange']/text()")
+        doi = selector.xpath("//a[@class='issue-item__doi']/text()")
+        pub_data = selector.xpath("//span[@class='CitationCoverDate']/text()")
+        abstract = selector.xpath("//span[@class='abstractSection']/text()")
+
+        info = {
+            "title": title,
+            "author": author,
+            "conference": conference,
+            "date": date,
+            "page": page,
+            "doi": doi,
+            "pub_data": pub_data,
+            "abstract": abstract,
+        }
+        print(info)
 
