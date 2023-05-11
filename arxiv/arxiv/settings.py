cbacd9e8bfa55d0b5bb2c89982012560dce0695c
顾剑波 <gujianbobo@gmail.com>
2023-05-11 15:53:23 +0800

    item

--- arxiv/arxiv/settings.py
+++ arxiv/arxiv/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "arxiv.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+# USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
